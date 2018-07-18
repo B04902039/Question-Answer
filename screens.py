3458c438289c25c2388a47bac7cb2ed441be03df
B04902039 <b04902039@ntu.edu.tw>
2018-07-18 21:35:07 +0800

    add countdown feature

--- screens.py
+++ screens.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from utils import *
 
-school_locations = ['校門口', '行政大', '校史館', '文學院', '土木系館', '活大', '農業陳列館', '小福', 
-'教學館', '小小福', '椰林小舖', '醉月湖', '臺大農場', '社科院', '總圖', '法學院', '工綜', '二活', 
-'桃花心木道', '體育館', '118巷', '溫州街', '公館商圈', '水源校區', '城中校區', '舟山路', '學生宿舍', 
-'N號館', '實驗林場']
+school_locations = ['起點', '校門口', 'N號館', '傅鐘', '校史館', '行政大樓', '文學院', '機會', 
+'溫州街', '社科院', '小福', '工綜', '農業陳列館', '醉月湖', '水源校區', '機會', '法學院', '活大', 
+'118巷', '機會', '土木系館', '總圖', '機會', '教學館', '城中校區', '體育館', '公館商圈', 
+'實驗林場', '臺大農場', '舟山路', '小小福', '二活', '機會', '椰林小舖', '宿舍區', '桃花心木道']
 questions={}
+gameBroad = board(school_locations)
 
 class LocationScreen(Screen):
     def __init__(self, **kwargs):
@@ -28,19 +29,30 @@ class LocationScreen(Screen):
 class QuestionScreen(Screen):
     current_ques = ObjectProperty()
     correct_id = NumericProperty()
+    limit = NumericProperty(10)
     loc = StringProperty()
     Qs = StringProperty()
     c1 = StringProperty()
     c2 = StringProperty()
     c3 = StringProperty()
     c4 = StringProperty()
+    cd = ObjectProperty()
     description = StringProperty()
     def __init__(self, **kwargs):
         super(QuestionScreen, self).__init__(**kwargs)
 
+    def tic(self, dt):
+        if self.limit > 0.1:
+            self.limit -= 0.1
+        else:
+            self.cd.cancel()
+
     def update(self):
         tmp = questions[self.loc]
         if len(tmp) == 0:
+            self.limit = 10
+            if self.cd:
+                self.cd.cancel()
             self.Qs = self.c1 = self.c2 = self.c3 = self.c4 = 'Out of questions'
         else:
             #Logger.info(tmp)
@@ -57,13 +69,21 @@ class QuestionScreen(Screen):
             self.c4 = self.current_ques[3]
             global questions    # question asked, delete it
             questions[self.loc].remove(questions[self.loc][idx])
+            self.limit = 10
+            if self.cd:
+                self.cd.cancel()
+            self.cd = Clock.schedule_interval(self.tic, 0.1)
 
     def callback(self, id):
         if id == self.correct_id:
+            self.manager.get_screen('map').update(correct=True)
             self.manager.get_screen('correctAnswer').description = self.description
             self.manager.transition.direction = 'up'
             self.manager.current = 'correctAnswer'
         else:
+            correct_answer = self.current_ques[self.correct_id]
+            self.manager.get_screen('map').update(correct=False)
+            self.manager.get_screen('wrongAnswer').correct_answer = correct_answer
             self.manager.get_screen('wrongAnswer').description = self.description
             self.manager.transition.direction = 'down'
             self.manager.current = 'wrongAnswer'
@@ -75,16 +95,19 @@ class CorrectAnswerScreen(Screen):
     
     def callback(self):
         Logger.info(self.description)
-        self.manager.current = 'location'
+        self.manager.get_screen('map').enter()
+        self.manager.current = 'map'
 
 class WrongAnswerScreen(Screen):
+    correct_answer = StringProperty()
     description = StringProperty()
     def __init__(self, **kwargs):
         super(WrongAnswerScreen, self).__init__(**kwargs)
     
     def callback(self):
         Logger.info(self.description)
-        self.manager.current = 'location'
+        self.manager.get_screen('map').enter()
+        self.manager.current = 'map'
 
 class TitleScreen(Screen):
     def __init__(self, **kwargs):
@@ -95,18 +118,18 @@ class TitleScreen(Screen):
     
     def startGame(self):
         self.manager.current = 'map'
-        self.manager.get_screen('map').enter(1)
+        self.manager.get_screen('map').enter()
 
 class InfoScreen(Screen):
     def __init__(self, **kwargs):
         super(InfoScreen, self).__init__(**kwargs)
 
     def goback(self):
-        self.manager.current = 'map'
-        self.manager.get_screen('map').enter(2)
+        self.manager.current = 'title'
     
     def startGame(self):
-        self.manager.current = 'location'
+        self.manager.current = 'map'
+        self.manager.get_screen('map').enter()
 
 class MapScreen(Screen):
     currentPlayer = NumericProperty()
@@ -115,14 +138,17 @@ class MapScreen(Screen):
     diceSum = StringProperty()
     def __init__(self, **kwargs):
         super(MapScreen, self).__init__(**kwargs)
+        self.currentPlayer = -1
     
-    def enter(self, n):
-        self.currentPlayer = n
+    def enter(self):
+        self.currentPlayer += 1
+        self.currentPlayer %= 6
         turnPop = Popup(title = 'Next!',
-                        content = Label(text = '第{}組的回合!'.format(n), 
+                        content = Label(text = '第{}組的回合!'.format(self.currentPlayer+1),
                         font_name = 'data/DroidSansFallback.ttf', 
                         font_size = 32),
-                        size_hint = (1,.3))
+                        size_hint = (.6,.3))
+        Clock.schedule_once(turnPop.dismiss, 1)
         turnPop.open()
     
     def rollDice(self):
@@ -130,3 +156,31 @@ class MapScreen(Screen):
         self.dice2 = randint(1, 6)
         self.diceSum = str(self.dice1 + self.dice2)
         Logger.info(self.diceSum)
+        # move chess
+        self.moveChess(self.currentPlayer, self.dice1+self.dice2)
+        next_loc = school_locations[gameBroad.players[self.currentPlayer].current_location]
+        rulePop = Popup(title = self.diceSum,
+                        content = Label(text = '第{}組前進{}格,到{}'.format(self.currentPlayer+1, self.diceSum, next_loc),
+                        font_name = 'data/DroidSansFallback.ttf', 
+                        font_size = 32),                   
+                        size_hint = (.6, .3))
+        Clock.schedule_once(rulePop.dismiss, 1)
+        if next_loc in questions.keys():
+            rulePop.bind(on_dismiss = lambda x: self.startQuestion(self.currentPlayer, next_loc))
+        else:
+            self.enter()
+        rulePop.open()
+
+    def update(self, correct):
+        pass
+
+    def moveChess(self, player_id, moves):
+        gameBroad.move_chess(player_id, moves)
+
+    def startQuestion(self, player_id, loc):
+        self.manager.get_screen('question').loc = loc
+        self.manager.get_screen('question').update()
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'question'
+
+    
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import kivy
 kivy.require('1.10.0')
-import math
+import math, time
 from random import randint, shuffle
 from functools import partial
 from kivy.app import App
@@ -14,9 +14,16 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty, StringProperty, NumericProperty
 from kivy.logger import Logger
+from kivy.animation import Animation
 from kivy.uix.popup import Popup
+from kivy.clock import Clock
 import kivy.resources
 kivy.resources.resource_add_path('.')
+
+school_locations = ['起點', '校門口', 'N號館', '傅鐘', '校史館', '行政大樓', '文學院', '機會', 
+'溫州街', '社科院', '小福', '工綜', '農業陳列館', '醉月湖', '水源校區', '機會', '法學院', '活大', 
+'118巷', '機會', '土木系館', '總圖', '機會', '教學館', '城中校區', '體育館', '公館商圈', 
+'實驗林場', '臺大農場', '舟山路', '小小福', '二活', '機會', '椰林小舖', '宿舍區', '桃花心木道']
 
 def shuffleChoice(choice):
     # shuffle the list in place and return the index of the true answer
@@ -26,3 +33,50 @@ def shuffleChoice(choice):
         if c == correct:
             Logger.info('{}, {}'.format(i, c))
             return i
+
+def auto_close(my_callback, sec):
+    Clock.schedule_once(my_callback, sec)
+
+class block(object):
+    location_name = ''
+    id = -1
+    current_player = set()
+    current_domination = {
+        'player': '',
+        'status': 0,
+    }
+    def __init__(self, i, name):
+        self.id = i
+        self.location_name = name
+
+class player(object):
+    id = -1
+    current_location = -1
+    def __init__(self, i):
+        self.id = i
+        self.current_location = 0
+    
+    def __str__(self):
+        return str((self.id, self.current_location))
+    def __repr__(self):
+        return str((self.id, self.current_location))
+
+class board(object):
+    blocks = []
+    players = []
+    def __init__(self, locations):
+        for i, loc in enumerate(locations):
+            self.blocks.append(block(i, loc))
+            for j in range(6):  # all player at starting point
+                self.blocks[0].current_player.add(j)
+        for i in range(6):
+            self.players.append(player(i))
+
+    def move_chess(self, player_id, moves):
+        # player on old block leave
+        self.blocks[self.players[player_id].current_location].current_player.remove(player_id)
+        self.players[player_id].current_location += moves
+        self.players[player_id].current_location %= len(self.blocks)
+        # player arrive on new block
+        self.blocks[self.players[player_id].current_location].current_player.add(player_id)
+        Logger.info(self.players)
